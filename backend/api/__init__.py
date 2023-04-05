89683435a3626ba4250ff7f1c5b22e6d8f887480
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-04-05 16:41:04 -0400

    update quiz

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,55 +1,11 @@
 from flask import Flask
 from api.lib.orm import find_all, build_from_record, find
 from api.lib.db import get_db
-from api.models.person import Person
-from api.models.address import Address
+
 import json
 import psycopg2
 
 def create_app(db_name):
     app = Flask(__name__)
     app.config.from_mapping(DATABASE = db_name)
-
-    @app.route('/')
-    def root_url():
-        return "welcome to the adventureworks app"
-    
-    @app.route('/persons')
-    def persons():
-        conn = get_db()
-        cursor = conn.cursor()
-        persons = find_all(cursor, Person)
-        person_dicts = [person.__dict__ for person in persons]
-        return json.dumps(person_dicts, default = str)
-    
-    @app.route('/persons/lastname/<lastname>')
-    def get_persons(lastname):
-        conn = get_db()
-        cursor = conn.cursor()
-        
-        cursor.execute('select * from person.person where lastname = %s', (lastname,))
-        records = cursor.fetchall()
-        persons = [build_from_record(Person, record) for record in records]
-        person_dicts = [person.__dict__ for person in persons]
-        return json.dumps(person_dicts, default = str)
-    
-    @app.route('/addresses')
-    def addresses():
-        conn = get_db()
-        cursor = conn.cursor()
-        addresses = find_all(cursor, Address)
-        address_dicts = [address.__dict__ for address in addresses]
-        return json.dumps(address_dicts, default = str)
-
-    @app.route('/person/addresses/<businessentityid>')
-    def person_with_addresses(businessentityid):
-        conn = get_db()
-        cursor = conn.cursor()
-        person = find(cursor, Person, businessentityid)
-        
-        person_json = person.to_json(cursor)
-        return json.dumps(person_json, default = str)
-
-
-
     return app
--- backend/api/lib/orm.py
+++ backend/api/lib/orm.py
@@ -1,18 +1,3 @@
-def find(cursor, cls, id):
-    
-    query = f'select * from {cls.__table__} where businessentityid = %s'
-    
-    cursor.execute(query, (id,))
-    record = cursor.fetchone()
-    return build_from_record(cls, record)
-
-def find_all(cursor, cls, limit = 10):
-    cursor.execute(f'select * from {cls.__table__} limit {limit}')
-    records = cursor.fetchall()
-    objs = [build_from_record(cls, record) for record in records]
-    return objs
-
-
 
 def build_from_record(Class, record):
     if not record: return None
